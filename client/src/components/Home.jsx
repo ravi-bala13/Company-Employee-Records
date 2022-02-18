5484a525ba24f3b2ba40fc82cfcf272e0752e0ba
Priya_darshini_s <priya06200@gmail.com>
2022-02-18 17:56:25 +0530

    updates

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,19 @@
+// import { useContext, useState } from "react"
+// import { useNavigate } from "react-router-dom";
+// import { AuthContext } from "../context/AuthContext";
+
+import "../CSS/Home.css"
+
+
+export const Home = () => {
+   
+
+    return <div>
+        <div id="screen">
+            <div id="homepage">
+
+            </div>
+
+        </div>
+    </div>
+}
