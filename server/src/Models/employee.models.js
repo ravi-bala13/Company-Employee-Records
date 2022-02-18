ae41a12dcfe17c2e48671c0e44f71171a472bed6
Priya_darshini_s <priya06200@gmail.com>
2022-02-18 17:40:55 +0530

    Update employee.models.js

---
+++ server/src/Models/employee.models.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema(
+    {
+        first_name: { type: String, required: true },
+        last_name: { type: String, required: true },
+        department: { type: String, required: true },
+        gender: { type: String, required: true, default: "Male"},
+        salary: { type: Number, required: true },
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+const Employee = mongoose.model("employee", employeeSchema);
+
+module.exports = Employee;
